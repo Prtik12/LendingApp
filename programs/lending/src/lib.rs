4a0ca2b4e76c1f5d2209db207483b60bdf3245bc
babyJesus12 <babyjesus7695@gmail.com>
2025-07-07 04:35:15 +0530

    admin.rs

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,7 +1,8 @@
 use anchor_lang::prelude::*;
-use instruction::*;
+use crate::instructions::*;
 
 mod state;
+mod instructions;
 
 declare_id!("rcVshHFGxjB6Tq9fZusUSzEAuC6YkFFRAc2M4KngHk2");
 
@@ -9,6 +10,13 @@ declare_id!("rcVshHFGxjB6Tq9fZusUSzEAuC6YkFFRAc2M4KngHk2");
 pub mod lending {
     use super::*;
 
+    pub fn init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) ->  Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv) 
+    }
+
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
 }
 
  
