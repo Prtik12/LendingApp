3c9208f62090e9eb7db558dabb1111ecb165e8f1
babyJesus12 <babyjesus7695@gmail.com>
2025-07-07 03:44:22 +0530

    fn process_init_bank

--- programs/lending/src/instructions/admin.rs
+++ programs/lending/src/instructions/admin.rs
@@ -33,3 +33,12 @@ pub struct InitBank<'info> {
     pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
 }
+
+pub fn process_init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
+    let bank = &mut ctx.accounts.bank;
+    bank.mint_address = ctx.accounts.mint.key();
+    bank.authority = ctx.accounts.signer.key();
+    bank.liquidation_threshold = liquidation_threshold;
+    bank.max_ltv = max_ltv;
+    Ok(())
+}
