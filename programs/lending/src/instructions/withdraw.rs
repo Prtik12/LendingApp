26b58a78f4ca9f360f9f486d56ff57bebcff3a27
babyJesus12 <babyjesus7695@gmail.com>
2025-07-11 02:18:04 +0530

    init-if-needed

---
+++ programs/lending/src/instructions/withdraw.rs
@@ -0,0 +1,37 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount};
+
+use crate::state::{Bank, User};
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        seeds = [mint.key().as_ref()],
+        bump,
+    )]
+    pub bank: Account<'info, Bank>,
+
+    #[account(
+        mut, 
+        seeds = [b"treasury", mint.key().as_ref()],
+        bump,       
+    )]
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [signer.key().as_ref()],
+        bump,
+    )]
+    pub user_account: Account<'info, User>,
+
+    #[account(
+        init_if_needed,
+    )]
+}
