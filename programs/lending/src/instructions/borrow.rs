76e291d4f4e2ae32596ce5d0a208dee13247df02
babyJesus12 <babyjesus7695@gmail.com>
2025-07-13 11:18:49 +0530

    process borrow instruction

--- programs/lending/src/instructions/borrow.rs
+++ programs/lending/src/instructions/borrow.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::Mint;
+use anchor_spl::token_interface::{Mint, TokenAccount};
+
+use crate::state::Bank;
 
 
 #[derive(Accounts)]
@@ -8,4 +10,37 @@ pub struct Borrow<'info> {
     pub signer: Signer<'info>,
 
     pub mint: Interface<'info, Mint>,
+
+    #[account(
+        mut,
+        seeds = [mint.key().as_ref()],
+        bump,
+    )]
+    pub bank: Account<'info, Bank>,
+
+    #[account(
+        mut, 
+        seeds = [b"treasury", mint.key().as_ref()],
+        bump,       
+    )]
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [signer.key().as_ref()],
+        bump,
+    )]
+    pub user_account: Account<'info, User>,
+
+    #[account(
+        init_if_needed,
+        payer = signer,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+        associated_token::token_program = token_program, 
+    )]
+    pub user_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
 }
